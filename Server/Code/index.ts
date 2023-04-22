0dd54ae8070905334508dfe79a01ef2f906733fd
BassHound <basso.andrea.96@gmail.com>
2023-04-22 17:28:52 +0200

    first draft of /login endpoint

--- Server/Code/index.ts
+++ Server/Code/index.ts
@@ -2,7 +2,6 @@ import {newUser} from "./Models/User";
 
 const dotenv = require('dotenv').config();
 import colors = require('colors');
-
 colors.enabled = true;
 if (dotenv.error) {
     console.log("Unable to load \".env\" file. Please provide one to store the JWT secret key".red);
@@ -12,22 +11,26 @@ if (dotenv.error) {
     process.exit(-1);
 }
 import * as mongoose from 'mongoose';
-
 const express = require('express');
 import * as http from "http";
-
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { expressjwt: jwt } = require('express-jwt');
+import jsonwebtoken = require('jsonwebtoken');  // JWT generation
 import * as user from './Models/User';
 
 
 const app = express();
+let auth = jwt( {
+    secret: process.env.JWT_SECRET,
+    algorithms: ["HS256"]
+} );
 app.use(cors());
 app.use(bodyParser.json());
-app.get('/', function (req: any, res: any) {
+app.get('/', function (req, res) {
     res.status(200).json({api_version: '1.1', author: 'BassHound'});
 });
-app.post('/users', (req: any, res: any, next: any) => {
+app.post('/users', (req, res, next) => {
 
     var u = user.newUser(req.body);
     if (!req.body.password) {
@@ -37,13 +40,48 @@ app.post('/users', (req: any, res: any, next: any) => {
 
     u.save().then((data: any) => {
         return res.status(200).json({error: false, errormessage: "", id: data._id});
-    }).catch((reason: any) => {
+    }).catch((reason) => {
         if (reason.code === 11000)
             return next({statusCode: 404, error: true, errormessage: "User already exists"});
         return next({statusCode: 404, error: true, errormessage: "DB error: " + reason.errmsg});
     })
-
 });
+
+app.get('/login', (req, res, next)=>{
+    let username = req.body.username;
+    let password = req.body.password;
+    console.log("New login attempt from ".green + username );
+    user.getModel().findOne( {mail: username} , (err, user)=> {
+        if (err) {
+            return next({statusCode: 500, error: true, errormessage: err});
+        }
+        if (!user) {
+            return next({statusCode: 500, error: true, errormessage: "Invalid user"});
+        }
+        if (user.validatePassword(password)) {
+            let tokendata = {
+                // TODO define token
+            };
+
+            console.log("Login granted. Generating token");
+            let token_signed = jsonwebtoken.sign(
+                tokendata,
+                process.env.JWT_SECRET as jsonwebtoken.Secret,
+                { expiresIn: '1h'}
+            );
+
+            // https://jwt.io
+            return res.status(200).json({
+                error: false,
+                errormessage: "",
+                id: user._id,
+                token: token_signed
+            });
+        }
+        return next({statusCode: 500, error: true, errormessage: "Invalid password"});
+    });
+});
+
 // the ENV var DBHOST is set only if the server is running inside a container
 const dbHost = process.env.DBHOST || '127.0.0.1';
 mongoose.connect('mongodb://' + dbHost + ':27017/taw-app2023').then(() => {
